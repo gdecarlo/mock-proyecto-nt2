932d04fb0c170bf56ded8316e4623c59b0f1c9bc
Gaspar De Carlo <gdecarlo@cetapsa.com>
2024-10-21 11:49:51 -0300

    se crea un componente para el header

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,7 @@
 <template>
   <div class="min-h-screen bg-gray-100">
-    <nav class="navbar">
-      <div class="navbar-links">
-        <a href="#">Rol usuario</a>
-        <a href="#">Dashboard</a>
-        <a href="#" class="ver-carrito">
-          Ver carrito
-          <span class="cart-count">3</span>
-        </a>
-        <a href="#" class="salir">Salir</a>
-      </div>
-    </nav>
-
+    
+    <AppHeader></AppHeader>
     <div class="container">
     <!-- Filtros en la columna izquierda -->
     <AppFilters class="FilterColumn"></AppFilters>
@@ -27,6 +17,7 @@
 import { ref } from 'vue'
 import AppFilters from './components/AppFilters.vue';
 import AppProducts from './components/AppProducts.vue';
+import AppHeader from './components/AppHeader.vue';
 
 </script>
 
@@ -38,34 +29,7 @@ body {
   color: #484848;
   background-color: #f7f7f7;
 }
-.navbar {
-  background-color: #00a699;
-  padding: 0 24px;
-  height: 80px;
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  box-shadow: 0 2px 4px rgba(0,0,0,0.1);
-}
-.navbar-links {
-  display: flex;
-  gap: 24px;
-  align-items: center;
-  flex-grow: 1;
-}
-.navbar-links a {
-  color: #ffffff;
-  text-decoration: none;
-  font-size: 16px;
-  font-weight: 600;
-  transition: color 0.3s ease;
-}
-.navbar-links a:hover {
-  color: #ffd700;
-}
-.salir {
-  margin-left: auto;
-}
+
 .container {
   display: flex;
   max-width: 1200px;
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,45 @@
+<template>
+    <nav class="navbar">
+      <div class="navbar-links">
+        <a href="#">Rol usuario</a>
+        <a href="#">Dashboard</a>
+        <a href="#" class="ver-carrito">
+          Ver carrito
+          <span class="cart-count">3</span>
+        </a>
+        <a href="#" class="salir">Salir</a>
+      </div>
+    </nav>
+
+</template>
+
+<style>
+.navbar {
+  background-color: #00a699;
+  padding: 0 24px;
+  height: 80px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  box-shadow: 0 2px 4px rgba(0,0,0,0.1);
+}
+.navbar-links {
+  display: flex;
+  gap: 24px;
+  align-items: center;
+  flex-grow: 1;
+}
+.navbar-links a {
+  color: #ffffff;
+  text-decoration: none;
+  font-size: 16px;
+  font-weight: 600;
+  transition: color 0.3s ease;
+}
+.navbar-links a:hover {
+  color: #ffd700;
+}
+.salir {
+  margin-left: auto;
+}
+</style>
